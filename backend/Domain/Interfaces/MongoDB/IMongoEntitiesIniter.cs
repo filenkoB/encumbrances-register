d92cdbd5462afe3a944010e6cfccd1a6fa210689
filenkoB <bogdan0filenko@gmail.com>
2021-11-26 22:13:11 +0200

    Added MongoDB connection factory

---
+++ backend/Domain/Interfaces/MongoDB/IMongoEntitiesIniter.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Driver;
+using System.Linq;
+
+namespace Domain.Interfaces.MongoDB
+{
+    public interface IMongoEntitiesIniter<TEntity, TClass>
+    {
+        public void InitCollection(IMongoCollection<TEntity> collection, IMongoDatabase db)
+        {
+            collection = db.GetCollection<TEntity>(
+               string.Join("", typeof(TClass).Name.Except("IMongo"))
+            );
+        }
+    }
+}
