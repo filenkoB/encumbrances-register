a4589e27c1e8f3a0979e1514b32f48706e59036a
Kostya <zverevfpm@gmail.comm>
2021-12-22 01:09:06 +0200

    lalalla

--- src/components/SystemRegApplication.vue
+++ src/components/SystemRegApplication.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <pre>{{application}}</pre>
     <div class="p-3 border border-1 rounded row">
       <div class="row text-center"><p>ПІБ та дата народження</p></div>
       <div class="row">
@@ -13,7 +14,7 @@
         </div>
         <div class="col">
           <div class="row">По-батькові:</div>
-          <div class="row">{{application.parentName}}</div>
+          <div class="row">{{application.patronymic}}</div>
         </div>
         <div class="col">
           <div class="row">Дата народження:</div>
@@ -24,22 +25,21 @@
     <div class="p-3 border border-1 rounded row">
       <div class="row text-center"><p>Паспорні дані</p></div>
       <div class="row">
-        <div v-if="application.chosenPassType === 'id-card'" class="col"><div class="row">Номер документа:</div><div class="row">{{application.pasNumber}}</div></div>
+        <div v-if="application.chosenPassType === 'id-card'" class="col">
+          <div class="row">Номер документа:</div>
+          <div class="row">{{application.passportNumber}}</div>
+        </div>
         <div v-else class="col">
           <div class="row">Серія та номер документа:</div>
-          <div class="row">{{application.pasSeria}} {{application.pasNumber}}</div>
+          <div class="row">{{application.passportInfo.serialNumber}} {{application.passportNumber}}</div>
         </div>
         <div class="col">
           <div class="row">Орган, що видав документ:</div>
-          <div class="row">{{application.pasAgency}}</div>
+          <div class="row">{{application.passportInfo.authorityId}}</div>
         </div>
         <div class="col">
           <div class="row">Дата видачі документа:</div>
-          <div class="row">{{application.pasDate}}</div>
-        </div>
-        <div class="col">
-          <div class="row">Номер картки платника податків:</div>
-          <div class="row">{{application.idNumber}}</div>
+          <div class="row">{{application.passportInfo.date}}</div>
         </div>
       </div>
     </div>
@@ -47,41 +47,41 @@
       <div class="row text-center"><p>Інформація про користувача системи</p></div>
       <div class="row">
         <div class="col">Електронна адреса: {{application.email}}</div>
-        <div class="col">Роль у Реєстрі: {{application.chosenRole}}</div>
-        <div v-if="application.chosenRole === 'user' && application.userIsAuthorized" class="col">Працює в уповноваженому органі</div>
-        <div v-else-if="application.chosenRole === 'user' && !application.userIsAuthorized" class="col">Не працює в уповноваженому органі</div>
-        <div v-if="application.chosenRole === 'user' || application.userIsAuthorized" class="col">Державний орган: {{application.agency}}</div>
+        <div class="col">Роль у Реєстрі: {{application.userType}}</div>
+        <div v-if="application.chosenRole === 'user' && application.authorityAddress" class="col">Працює в уповноваженому органі</div>
+        <div v-else-if="application.chosenRole === 'user' && !application.authorityAddress" class="col">Не працює в уповноваженому органі</div>
       </div>
-      <div class="border border-1 rounded border-primary mt-3">
+      <div class="border border-1 rounded border-primary mt-3" v-if="application.authorityAddress">
         <div class="row mt-2"><p>Адреса уповноваженого органу:</p></div>
-        <div class="row">
-          <p>Країна: {{application.address.path.country}},
-          oбласть: {{application.address.path.region}},
-          pайон: {{application.address.path.district}},
-          місто: {{application.address.path.city}},
-          індекс: {{application.address.path.index}},
-          вулиця: {{application.address.path.street}},
-          будинок: {{application.address.path.build}},
-          корпус: {{application.address.path.corps}}.</p>
-        </div>
+        <Address :path="address.address.path" :editing_status="true"/>
       </div>
     </div>
   </div>    
 </template>
 
 <script>
-import {GetUserStatment} from '../connect_to_server'
+import {UserStatementsInfo} from '../connect_to_server'
+import Address from './statement_parts/Address.vue'
+import {EncumbranceTierDebtor} from '../classes'
 export default {
   data(){
     return {
-      application: null
+      application: null,
+      address: null
     }
   },
   name: "SystemRegApplication",
   props: ["item"],
-  
-  async beforeCreate(){
-    this.application = await GetUserStatment(this.item.id)
+  components:{
+    Address
+  },
+  async created(){
+    this.application = await UserStatementsInfo(this.item.id);
+    console.log(this.application);
+    if(this.application.authorityAddress != null)
+    {
+      this.address = new EncumbranceTierDebtor(null, null, null, null, this.application.authorityAddress);
+    }   
   }
 }
 </script>
