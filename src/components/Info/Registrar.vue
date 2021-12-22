fceaf3a9ab4860fd10ce0b47e1fea556fa35f4e1
Kostya <zverevfpm@gmail.comm>
2021-12-22 16:32:45 +0200

    lalalla

--- src/components/Info/Registrar.vue
+++ src/components/Info/Registrar.vue
@@ -18,12 +18,11 @@
                 <i class="fa fa-hand-o-right" aria-hidden="true"></i>
               </div>
               <div class="col-auto">
-                <p>Для перегляду списку поданий на реєстрацію "Заяв" скористайтеся розділом "Переглянути Заяви".</p>
+                Для перегляду списку поданий на реєстрацію "Заяв" скористайтеся розділом "Переглянути Заяви".</div>
               </div>
-            </div>
             <div class="row ms-5">
               <div class="col-auto">
-                <div class="row">
+                <div class="row mt-2">
                   <div class="col-auto mt-2">
                     <i class="fa fa-angle-right fa-lg" aria-hidden="true"></i>
                   </div>
@@ -31,7 +30,7 @@
                     <p>Ви можете знайти даний розділ в верхньому лівому куті головного меню.</p>
                   </div>
                   <div class="col-auto">
-                    <button type="button" class="btn btn-outline-dark" disabled>
+                    <button type="button" class="btn btn-outline-dark me-2" disabled>
                       <i class="fa fa-list-alt" aria-hidden="true"></i>
                       Переглянути Заяви
                     </button>
@@ -39,7 +38,7 @@
                 </div>
                 <div class="row ms-4">
                   <div class="col-auto mt-2">
-                    <p>Кожен елемент, що охарактеризовує "Заяву" має свій функціонал, а саме:</p>
+                    <p>Кожен елемент, що охарактеризовує подану на реєстрацію заявку має свій функціонал, а саме:</p>
                   </div>
                 </div>
                 <div class="row ms-4">
@@ -57,60 +56,56 @@
                   </div>
                 </div>
                 <div class="row ms-4 mt-2">
-                  <div class="col-auto mt-2">
-                    <i class="fa fa-angle-double-right fa-lg" aria-hidden="true"></i>
-                  </div>
-                  <div class="col-auto mt-2">
-                    Для підтвердження реєстріції відомостей "Заяви", натисніть
-                  </div>
-                  <div class="col-auto">
-                    <button type="button" class="btn btn-outline-success" disabled>
-                      Підтвердити
-                    </button>
-                  </div>
-                  <div class="col-auto mt-2">
-                    та підтвердьте або відхиліть вашу дію в спливаючому вікні.
-                  </div>
-                </div>
-                <div class="row ms-4 mt-2">
-                  <div class="col-auto mt-2">
-                    <i class="fa fa-angle-double-right fa-lg" aria-hidden="true"></i>
-                  </div>
-                  <div class="col-auto mt-2">
-                    Для відхилення реєстріції відомостей "Заяви", натисніть
-                  </div>
-                  <div class="col-auto">
-                    <button type="button" class="btn btn-outline-danger" disabled>
-                      Відхилити
-                    </button>
-                  </div>
-                  <div class="col-auto mt-2">
-                    та підтвердьте або відхиліть вашу дію в спливаючому вікні.
+                  <div class="col">
+                    <div class="row ms-4 mt-2">
+                      <div class="col-auto mt-2">
+                        <i class="fa fa-arrow-right" aria-hidden="true"></i>
+                      </div>
+                      <div class="col-auto mt-2">
+                         Для підтвердження реєстріції відомостей "Заяви", натисніть
+                      </div>
+                      <div class="col-auto">
+                        <button type="button" class="btn btn-outline-success" disabled>
+                          Підтвердити
+                        </button>
+                      </div>
+                    </div>
+                    <div class="row ms-4 mt-2">
+                      <div class="col-auto mt-2">
+                        <i class="fa fa-arrow-right" aria-hidden="true"></i>
+                      </div>
+                      <div class="col-auto mt-2">
+                         Для відхилення реєстріції відомостей "Заяви", натисніть
+                      </div>
+                      <div class="col-auto">
+                        <button type="button" class="btn btn-outline-danger" disabled>
+                          Відхилити
+                        </button>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </li>
-          <li class="list-group-item">
-            <div>
-              <i class="fa fa-hand-o-right" aria-hidden="true"></i>
-              Для перегляду активності "Реєстраторів" скористайтеся розділом "Перелік Реєстраторів".
-            </div>
-            <div class="ms-3 mt-2">
-              <i class="fa fa-chevron-right" aria-hidden="true"></i>
-              Ви можете знайти даний розділ в верхньому лівому куті головного меню.
-              <button type="button" class="btn btn-outline-dark ms-2" disabled>
-                Перелік Реєстраторів
-              </button>
-            </div>
-          </li>
+          <Create />
+          <search />
+          <main-info />
         </ul>
       </div>
     </div>
   </div>
 </template>
 <script>
+import MainInfo from './maininfo.vue'
+import Create from './create.vue'
+import Search from './search.vue'
 export default {
     name: 'Registrar',
+    components:{
+      MainInfo,
+      Create,
+      Search
+    }
 }
 </script>
---
+++ src/components/Info/search.vue
@@ -0,0 +1,60 @@
+<template>
+  <li class="list-group-item">
+    <div class="row">
+        <div class="col-auto">
+        <i class="fa fa-hand-o-right" aria-hidden="true"></i>
+        </div>
+        <div class="col-auto">
+        Для пошуку обтяження скористайтеся розділом "Пошук обтяження".</div>
+    </div>
+    <div class="row ms-5">
+      <div class="col-auto">
+        <div class="row mt-2">
+          <div class="col-auto mt-2">
+            <i class="fa fa-angle-right fa-lg" aria-hidden="true"></i>
+            </div>
+            <div class="col-auto mt-2">
+            <p>Ви можете знайти даний розділ в верхньому лівому куті головного меню.</p>
+            </div>
+            <div class="col-auto">
+            <button type="button" class="btn btn-outline-dark me-2" disabled>
+              <i class="fa fa-search" aria-hidden="true"></i>
+              Пошук обтяження
+            </button>
+          </div>
+        </div>
+        <div class="row ms-4 mt-2">
+          <div class="col-auto mt-2">
+            <i class="fa fa-angle-double-right fa-lg" aria-hidden="true"></i>
+          </div>
+          <div class="col-auto mt-2">
+            Для проведення перевірки введених даних та подальшого пошуку обтяжень, натисніть
+          </div>
+          <div class="col-auto">
+            <button type="button" class="btn btn-outline-success" disabled>
+              Підтвердити
+            </button>
+          </div>
+        </div>
+        <div class="row ms-4 mt-2">
+          <div class="col-auto mt-2">
+            <i class="fa fa-angle-double-right fa-lg" aria-hidden="true"></i>
+          </div>
+          <div class="col-auto mt-2">
+            Для очищення заповнених полів, натисніть
+          </div>
+          <div class="col-auto">
+            <button type="button" class="btn btn-outline-danger" disabled>
+              Очистити
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </li>
+</template>
+<script>
+export default {
+    name:"Search"
+}
+</script>
